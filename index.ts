41672bfaaa667fd5e8fb6a9c938282298b1986d5
Death Stalker <jawadvamps@gmail.com>
2026-09-01 23:51:40 +0330

    trying to last output

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+// index.ts
 import { I18nManager } from 'react-native';
 
+console.log('BEFORE:', I18nManager.isRTL);
 I18nManager.allowRTL(false);
 I18nManager.forceRTL(false);
+console.log('AFTER forceRTL call:', I18nManager.isRTL);
 
 require('expo-router/entry');
